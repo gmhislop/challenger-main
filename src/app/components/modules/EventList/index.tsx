263313272cb8a5beb0a7b96a73b13a3c8b8a0dcb
Giovanni Hislop <89380684+gmhislop@users.noreply.github.com>
2024-03-14 22:34:44 +0100

    [Improvements] - No results

--- src/app/components/modules/EventList/index.tsx
+++ src/app/components/modules/EventList/index.tsx
@@ -7,7 +7,6 @@ import React from 'react';
 import { useEventList } from './EventList.hooks';
 import { SearchBar } from '../../common/SearchBar';
 
-
 export const EventList = ({ allEvents }: Readonly<{ allEvents: i.Event[] }>) => {
     const { events, handleSearchChange } = useEventList(allEvents);
 
@@ -22,48 +21,54 @@ export const EventList = ({ allEvents }: Readonly<{ allEvents: i.Event[] }>) => 
             <div className="flex justify-center items-center w-full py-6">
                 <SearchBar onSearchChange={handleSearchChange} />
             </div>
-            <motion.div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
-                {events.map((event, index) => (
-                    <motion.div
-                        key={index}
-                        variants={variants}
-                        initial="hidden"
-                        animate={'visible'}
-                        transition={{
-                            delay: (index / events.length) * stagger,
-                            ease: 'easeInOut',
-                            duration: 0.5,
-                        }}
-                        viewport={{ amount: 0 }}
-                    >
-                        <a
-                            className="relative aspect-video overflow-hidden"
-                            href={`/event/${event.id}`}
-                            key={event.id}
+            {events.length === 0 ? (
+                <div className="flex justify-center items-center w-full h-96">
+                    <h2>No results found, try another search term.</h2>
+                </div>
+            ) : (
+                <motion.div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+                    {events.map((event, index) => (
+                        <motion.div
+                            key={index}
+                            variants={variants}
+                            initial="hidden"
+                            animate={'visible'}
+                            transition={{
+                                delay: (index / events.length) * stagger,
+                                ease: 'easeInOut',
+                                duration: 0.5,
+                            }}
+                            viewport={{ amount: 0 }}
                         >
-                            <div className="absolute inset-x-0 bottom-0 p-2">
-                                <h1 className="text-sm text-primary-foreground">{event.name}</h1>
-                                <p className="text-xs text-secondary-foreground">
-                                    {event.location} -{' '}
-                                    {new Date(event.date).toLocaleDateString('en-US', {
-                                        day: '2-digit',
-                                        month: '2-digit',
-                                        year: 'numeric',
-                                    })}
-                                </p>
-                            </div>
+                            <a
+                                className="relative aspect-video overflow-hidden"
+                                href={`/event/${event.id}`}
+                                key={event.id}
+                            >
+                                <div className="absolute inset-x-0 bottom-0 p-2">
+                                    <h1 className="text-sm text-primary-foreground">{event.name}</h1>
+                                    <p className="text-xs text-secondary-foreground">
+                                        {event.location} -{' '}
+                                        {new Date(event.date).toLocaleDateString('en-US', {
+                                            day: '2-digit',
+                                            month: '2-digit',
+                                            year: 'numeric',
+                                        })}
+                                    </p>
+                                </div>
 
-                            <Image
-                                className="object-cover h-full w-full"
-                                src={event.imageUrl}
-                                width={320}
-                                height={200}
-                                alt={event.name}
-                            />
-                        </a>
-                    </motion.div>
-                ))}
-            </motion.div>
+                                <Image
+                                    className="object-cover h-full w-full"
+                                    src={event.imageUrl}
+                                    width={320}
+                                    height={200}
+                                    alt={event.name}
+                                />
+                            </a>
+                        </motion.div>
+                    ))}
+                </motion.div>
+            )}
         </>
     );
 };
